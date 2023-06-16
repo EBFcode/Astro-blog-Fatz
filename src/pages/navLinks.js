5287f1825320dd033984b54a4131c781015dce87
Jhony Robles <92616695+EBFcode@users.noreply.github.com>
2023-06-16 01:14:09 -0400

    changes - 2

---
+++ src/pages/navLinks.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+        href: "/",
+        text: "Home",
+    },
+    {
+        href: "/about/",
+        text: "About",
+    },
+    {
+        href: "/blog/",
+        text: "Blog",
+    },
+];
